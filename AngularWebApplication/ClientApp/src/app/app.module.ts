8d227a28a9fa54d9f904b7311a4b22f390d64406
swalest <swalest@SWALEST-DEV>
2018-08-09 14:21:35 +0100

    Quizzes added to the menu!

--- AngularWebApplication/ClientApp/src/app/app.module.ts
+++ AngularWebApplication/ClientApp/src/app/app.module.ts
@@ -7,20 +7,26 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { QuizHomeComponent } from './quiz/quiz-home.component';
+import { QuizListComponent } from './quiz/quiz-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    QuizHomeComponent,
+    QuizListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path:'quiz-home', component: QuizHomeComponent}
     ])
   ],
   providers: [],
---
+++ AngularWebApplication/ClientApp/src/app/quiz/quiz-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quiz-home',
+  templateUrl: './quiz-home.component.html',
+})
+export class QuizHomeComponent {
+}
