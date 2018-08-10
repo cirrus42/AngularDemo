3db1b37dbd752b0ca396f0566a7880c4dcaa9de2
swalest <swalest@SWALEST-DEV>
2018-08-10 07:35:45 +0100

    working quiz list

--- AngularWebApplication/ClientApp/src/app/quiz/quiz-list.component.ts
+++ AngularWebApplication/ClientApp/src/app/quiz/quiz-list.component.ts
@@ -6,22 +6,25 @@ import { HttpClient } from "@angular/common/http";
   templateUrl: './quiz-list.component.html',
   styleUrls: ['./quiz-list.component.css']
 })
+
 export class QuizListComponent {
   title: string;
   selectedQuiz: IQuiz;
   quizzes: IQuiz[];
   theUrl: string;
-  public forecasts: IWeatherForecast[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.title = "Latest Quizzes";
     var url = baseUrl + "api/quiz/Latest/";
     this.theUrl = url;
 
-    //http.get<IQuiz[]>(url).subscribe(result => { this.quizzes = result; }, error => console.error(error));
-    http.get<IWeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
+    http.get<IQuiz[]>(url).subscribe(result => {
+       this.quizzes = result;
     }, error => console.error(error));
+  }
 
+  onSelect(quiz: IQuiz) {
+    this.selectedQuiz = quiz;
+    console.log("quiz with Id " + this.selectedQuiz.id + " has been selected.");
   }
 }
